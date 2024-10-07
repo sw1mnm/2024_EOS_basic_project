8ee1526b89454a963e1c15bcf723da6dcf1d63e7
sw1mnm <sooo0chewv@gmail.com>
2024-10-07 18:14:11 +0900

    1007

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eos_todolist/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,75 +18,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
-        title: Text('EOS ToDoList'),
-        leading: Image.asset('assets/images/eos_logo.png'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            padding: EdgeInsets.all(25),
-            child: Row(
-              children: [
-                Container(
-                  height: 140,
-                  width: 140,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 10,
-                      color: Colors.grey,
-                    ),
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(70),
-                  ),
-                  child: Center(
-                    child: Image.asset('assets/images/eos_logo.png'),
-                  ),
-                ),
-                SizedBox(
-                  width: 35,
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '최수영',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text('과탑이 될테야!!!!')
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 500,
-            color: Color(0xFFA4C639).withOpacity(0.1),
-          )
-        ],
-      ),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
+        title: Text('EOS ToDoList'),
+        leading: Image.asset('assets/images/eos_logo.png'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            padding: EdgeInsets.all(25),
+            child: Row(
+              children: [
+                Container(
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 10,
+                      color: Colors.grey,
+                    ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(70),
+                  ),
+                  child: Center(
+                    child: Image.asset('assets/images/eos_logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  width: 35,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '최수영',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text('과탑이 될테야!!!!')
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFA4C639).withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 20,
+                left: MediaQuery.of(context).size.width / 2 - 75,
+                child: Container(
+                  width: 150,
+                  height: 38,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFA4C639).withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "To do list",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
